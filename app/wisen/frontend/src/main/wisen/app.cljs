7262d7d5c80af32021dfd4abe48e39fe2d0a9688
Markus Schlegel <markus.schlegel@active-group.de>
2025-01-28 12:08:25 +0100

    Add Phoenix channel communication

--- app/wisen/frontend/src/main/wisen/app.cljs
+++ app/wisen/frontend/src/main/wisen/app.cljs
@@ -1,10 +1,26 @@
 (ns wisen.app
   (:require [reacl-c.core :as c :include-macros true]
             [reacl-c.dom :as dom :include-macros true]
-            [reacl-c.main :as cmain]))
+            [reacl-c.main :as cmain]
+            ["phoenix" :as phx]))
+
+
+(defonce socket
+  (new phx/Socket "/sock" #js {:params #js {:token js/window.-token}}))
 
 (defn init []
-  (println "Hi from ClojureScript"))
+  (js/console.log socket)
+  (.connect socket)
+  (let [channel (.channel socket "room:lobby" #js {})]
+    (js/console.log channel)
+    (-> channel
+        (.join)
+        (.receive "ok" (fn [resp]
+                         (println "SUCCESS")
+                         (println (pr-str resp))))
+        (.receive "error" (fn [resp]
+                            (println "ERROR")
+                            (println (pr-str resp)))))))
 
 (defn toplevel []
   (dom/strong "Hello!"))
--- app/wisen/lib/wisen_web/endpoint.ex
+++ app/wisen/lib/wisen_web/endpoint.ex
@@ -15,6 +15,8 @@ defmodule WisenWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
+  socket "/sock", WisenWeb.SockSocket, websocket: true, longpoll: false
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
