7fac952a6cb0f887484d81e920dda0f1b5fd926b
Markus Schlegel <markus.schlegel@active-group.de>
2025-01-28 09:14:48 +0100

    Rumspielen mit LiveView

--- app/wisen/lib/wisen/domain.ex
+++ app/wisen/lib/wisen/domain.ex
@@ -14,16 +14,23 @@ defmodule Wisen.Domain do
 
   # QuickStruct.define_module(Event, [:title, :when, :where])
 
-  QuickStruct.define_module(HostsEventProperty, [event: Event.t])
+  # QuickStruct.define_module(HostsEventProperty, [event: Event.t])
 
   QuickStruct.define_module(HasGoogleMapsProperty, [uri: String.t])
 
   QuickStruct.define_module(HasWebsiteProperty, [uri: String.t])
 
-  QuickStruct.define_module(Titled, [title: String.t])
-
   # Resource
 
-  QuickStruct.define_module(Resource, [:properties])
+  QuickStruct.define_module(Resource, [:title, :properties])
+
+  def set_title(resource, title) do
+    %Resource{resource | title: title}
+  end
+
+  def add_empty_caters_to(resource) do
+    prop = CatersToProperty.make(:elderly)
+    %Resource{resource | properties: resource.properties ++ [prop]}
+  end
 
 end
