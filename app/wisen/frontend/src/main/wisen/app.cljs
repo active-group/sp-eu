ffa0e7802cea4ffc872d3a50cb5ba53a7d099d1b
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-10 15:43:51 +0100

    Remove phx from frontend

--- app/wisen/frontend/src/main/wisen/app.cljs
+++ app/wisen/frontend/src/main/wisen/app.cljs
@@ -3,41 +3,10 @@
             [reacl-c.core :as c :include-macros true]
             [reacl-c.dom :as dom :include-macros true]
             [reacl-c.main :as cmain]
-            [wisen.resource :as r]
-            ["phoenix" :as phx]))
+            [wisen.resource :as r]))
 
 (defn init []
   (js/console.log "init"))
-
-(defonce socket
-  (new phx/Socket "/sock" #js {:params #js {:token js/window.-token}}))
-
-(defn channel-from-socket [sock]
-  (.connect sock)
-  (let [^phx/Channel channel-1 (.channel sock "room:lobby" #js {})
-        ^phx/Channel channel-2 (.join channel-1)
-        ^phx/Channel channel-3 (.receive channel-2
-                                         "ok"
-                                         (fn [resp]
-                                           (println "SUCCESS")
-                                           (println (pr-str resp))))
-        ^phx/Channel channel-4 (.receive channel-3
-                                         "error"
-                                         (fn [resp]
-                                           (println "ERROR")
-                                           (println (pr-str resp))))]
-
-    channel-1))
-
-(c/defn-subscription channel-on deliver! [^phx/Channel chann cmd]
-  (let [handler (fn [payload]
-                  (deliver! payload))]
-
-    (.on chann cmd handler)
-    #()))
-
-(c/defn-effect channel-push [^phx/Channel chann cmd js-payload]
-  (.push chann cmd js-payload))
 
 (c/defn-item query-form "has no public state" []
   (c/isolate-state
@@ -58,26 +27,14 @@
      (c/handle-action
       (query-form)
       (fn [st query]
-        (c/return :action (channel-push
+        (c/return :action
+                  ::TODO
+                  #_(channel-push
                            (:channel state)
                            "search"
-                           #js {:query query}))))
-
-     (c/focus :message
-              (c/handle-action
-               (channel-on (:channel state) "new-state")
-               (fn [st ac]
-                 (c/return :state (js->clj ac)))))
-
-     #_(dom/strong "Hello!")
-     #_(dom/button {:onclick (fn [_]
-                             (c/return :action (channel-push (:channel state)
-                                                             "new-msg"
-                                                             #js {:body "ja moin"})))}
-                 "Send a message"))))
+                           #js {:query query})))))))
 
 (cmain/run
   (.getElementById js/document "main")
   (toplevel)
-  {:initial-state {:socket socket
-                   :channel (channel-from-socket socket)}})
+  {:initial-state ::garnix})
