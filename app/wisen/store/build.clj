f536a05b9f2e8087e9b44242277b9c12352f5a72
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-10 15:15:35 +0100

    Version 0.1

--- app/wisen/store/build.clj
+++ app/wisen/store/build.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'wisen/store)
-(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def version (format "0.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
 
