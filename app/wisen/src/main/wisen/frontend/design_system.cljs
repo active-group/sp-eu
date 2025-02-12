fbbd1424e02a55abebad1a261ecf78405b5cc9e0
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-12 16:56:45 +0100

    Style

---
+++ app/wisen/src/main/wisen/frontend/design_system.cljs
@@ -0,0 +1,29 @@
+(ns wisen.frontend.design-system
+  (:require [reacl-c.core :as c :include-macros true]
+            [reacl-c.dom :as dom :include-macros true]
+            [active.clojure.lens :as lens]
+            [reacl-c-basics.forms.core :as forms]))
+
+(def border "1px solid #bbb")
+
+(dom/defn-dom card [attrs & children]
+  (apply dom/div
+         (dom/merge-attributes attrs
+                               {:style {#_#_:border border
+                                        :background "white"
+                                        :border-radius "4px"
+                                        :overflow "hidden"
+                                        :box-shadow "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)"}})
+         children))
+
+(dom/defn-dom with-card-padding [attrs & children]
+  (apply dom/div
+         (dom/merge-attributes attrs
+                               {:style {:padding "8px 16px"}})
+         children))
+
+(dom/defn-dom padded-1 [attrs & children]
+  (apply dom/div
+         (dom/merge-attributes attrs
+                               {:style {:padding "3px 8px"}})
+         children))
--- app/wisen/src/main/wisen/frontend/display.cljs
+++ app/wisen/src/main/wisen/frontend/display.cljs
@@ -2,21 +2,45 @@
   (:require [reacl-c.core :as c :include-macros true]
             [reacl-c.dom :as dom :include-macros true]
             [active.clojure.lens :as lens]
-            [reacl-c-basics.forms.core :as forms]))
+            [reacl-c-basics.forms.core :as forms]
+            [wisen.frontend.design-system :as ds]))
+
+(defn display-object [obj]
+  (cond
+    (sequential? obj)
+    (apply dom/ul (map display-object obj))
+
+    (and (map? obj)
+         (some? (get obj "@value")))
+    (get obj "@value")
+
+    :else
+    (pr-str obj)))
 
 (defn display-property [prop]
-  (dom/div
-   (dom/div "Predicate: " (pr-str (first prop)))
-   (dom/div "Object: " (pr-str (second prop)))))
+  (c/fragment
+   (dom/dt (first prop))
+   (dom/dd (display-object (second prop)))))
 
 (defn display-resource [res]
-  (dom/div
-   (dom/div
-    (dom/div "Resource Id: " (get res "@id"))
-    (dom/div "Properties:"
-             (apply dom/ul
-                    (map (comp dom/li display-property)
-                         (dissoc res "@id")))))))
+  (ds/card
+   (dom/div {:style {:display "flex"
+                     :justify-content "space-between"
+                     #_#_:border-bottom ds/border}}
+
+            (ds/padded-1
+             {:style {:color "hsl(229.18deg 91.04% 56.86%)"}}
+             "Resource")
+
+            (ds/padded-1
+             {:style {:color "#555"
+                      :font-size "14px"}}
+             (get res "@id")))
+
+   (ds/with-card-padding
+     (apply dom/dl
+            (map display-property
+                 (dissoc res "@id"))))))
 
 (c/defn-item display-expanded [result-json]
   (js/console.log result-json)
@@ -26,6 +50,12 @@
      ;; assume a list of resources
      (array? result-json)
      (apply dom/ul
+            {:style {:list-style-type "none"
+                     :padding 0
+                     :display "flex"
+                     :flex-direction "column"
+                     :gap "12px"}}
+
             (map (fn [res]
                    (dom/li
                     (display-resource (js->clj res))))
@@ -33,5 +63,4 @@
 
      ;; assume a single resource
      (object? result-json)
-     (display-resource (js->clj result-json)))
-   (pr-str result-json)))
+     (display-resource (js->clj result-json)))))
