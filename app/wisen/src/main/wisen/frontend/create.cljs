0c39bfc0d38e6a2f61b107cbc9d7b32f2b663924
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-13 12:40:44 +0100

    WIP resource component

--- app/wisen/src/main/wisen/frontend/create.cljs
+++ app/wisen/src/main/wisen/frontend/create.cljs
@@ -1,4 +1,24 @@
-(ns wisen.frontend.create)
+(ns wisen.frontend.create
+  (:require [wisen.frontend.resource :as r]
+            [wisen.frontend.resource-component :as rc]
+            [reacl-c.core :as c :include-macros true]
+            [reacl-c.dom :as dom]
+            [wisen.frontend.design-system :as ds]))
+
+(def sample-resource
+  (r/res
+   (r/prop r/id "http://example.org/hirsch")
+   (r/prop "http://schema.org/name" "Hirsch")
+   (r/prop "http://schema.org/email" "hirsch@example.org")
+   (r/prop "http://schema.org/url" "https://hirsch-begegnungsstaette.de")
+   (r/prop "http://schema.org/geo" (r/res
+                                    (r/prop "@type" "http://schema.org/GeoCoordinates")
+                                    (r/prop "http://schema.org/latitude" "48.52105844145676")
+                                    (r/prop "http://schema.org/longitude" "9.054090697517525")))))
 
 (defn main []
-  "TODO create")
+  (ds/padded-2
+   {:style {:overflow "auto"}}
+   (dom/h2 "Create a new resource")
+   (c/isolate-state sample-resource
+                    (rc/main))))
---
+++ app/wisen/src/main/wisen/frontend/resource_component.cljs
@@ -0,0 +1,81 @@
+(ns wisen.frontend.resource-component
+  (:require [wisen.frontend.resource :as r]
+            [reacl-c.core :as c :include-macros true]
+            [reacl-c.dom :as dom :include-macros true]
+            [active.clojure.lens :as lens]
+            [reacl-c-basics.forms.core :as forms]
+            [wisen.frontend.design-system :as ds]))
+
+(defn pr-predicate [p]
+  (case p
+    "http://schema.org/name"
+    "Name"
+
+    "http://schema.org/email"
+    "E-Mail"
+
+    "http://schema.org/url"
+    "Website"
+
+    "http://schema.org/geo"
+    "The geo coordinates of the place"
+
+    p))
+
+(declare part)
+
+(defn property-component []
+  (c/with-state-as property
+    (dom/div
+     (dom/div (pr-predicate (r/property-predicate property)))
+     (dom/div (c/focus r/property-object (part)))
+     )))
+
+(defn resource-component []
+  (c/focus r/resource-properties
+           (c/with-state-as properties
+             (ds/with-card-padding
+               (apply dom/div
+
+                      (interpose (dom/hr)
+                                 (map-indexed (fn [idx _]
+                                                (c/focus (lens/at-index idx)
+                                                         (dom/div
+                                                          {:style {:padding "8px 0"}}
+                                                          (property-component))))
+                                              properties)))))))
+
+(c/defn-item part []
+  (c/with-state-as x
+    (cond
+      (r/literal-string? x)
+      (dom/strong
+       (c/focus r/literal-string-value
+                (forms/input {:type "text"})))
+
+      (r/resource? x)
+      (resource-component))
+    ))
+
+
+(defn main []
+  (c/with-state-as resource
+    (ds/card
+     (dom/div {:style {:display "flex"
+                       :justify-content "space-between"
+                       :border-bottom ds/border}}
+
+              (ds/padded-1
+               {:style {:color "hsl(229.18deg 91.04% 56.86%)"}}
+               "Resource")
+
+              (ds/padded-1
+               {:style {:color "#555"
+                        :font-size "14px"}}
+               (if-let [uri (r/lookup resource r/id)]
+                 uri
+                 "Blank")))
+
+     (c/focus (r/dissoc r/id)
+              (resource-component)
+              ))))
