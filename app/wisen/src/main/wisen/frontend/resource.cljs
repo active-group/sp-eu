1dcfc868a43072876bd48f88b5977a4c15cc64fc
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-13 12:40:44 +0100

    WIP resource component

---
+++ app/wisen/src/main/wisen/frontend/resource.cljs
@@ -0,0 +1,63 @@
+(ns wisen.frontend.resource
+  (:refer-clojure :exclude [dissoc])
+  (:require [active.data.record :as record :refer-macros [def-record]]
+            [active.data.realm :as r]
+            [active.clojure.lens :as lens]))
+
+;; Some predicates
+
+(def id ::id)
+
+;; ---
+
+(defn literal-string? [x]
+  (string? x))
+
+(def literal-string-value lens/id)
+
+(declare resource)
+
+(def-record property
+  [property-predicate :- (r/union r/string
+                                  (r/enum id))
+
+   property-object :- (r/union r/string
+                               (r/delay resource))])
+
+(defn prop [p o]
+  (property
+   property-predicate p
+   property-object o))
+
+(defn property? [x]
+  (record/is-a? property x))
+
+(def-record resource
+  [resource-properties :- (r/sequence-of property)])
+
+(defn res [& props]
+  (resource resource-properties props))
+
+(defn resource? [x]
+  (record/is-a? resource x))
+
+(defn lookup [r pred]
+  (some (fn [prop]
+          (when (= pred (property-predicate prop))
+            (property-object prop)))
+        (resource-properties r)))
+
+#_(defn dissoc [r pred]
+  (lens/overhaul r resource-properties
+                 (fn [props]
+                   (remove #(= pred (property-predicate %)) props))))
+
+(defn dissoc [pred]
+  (fn
+    ([r]
+     (resource
+      resource-properties
+      (remove #(= pred (property-predicate %))
+              (resource-properties r))))
+    ([_ r]
+     r)))
