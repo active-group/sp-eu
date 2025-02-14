75614599ef4e7cc5fc5f4af3ff100aea14204cdb
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-14 16:46:04 +0100

    leaflet circle for GeoRadius

--- app/wisen/src/main/wisen/frontend/leaflet.cljs
+++ app/wisen/src/main/wisen/frontend/leaflet.cljs
@@ -5,7 +5,7 @@
 
 (def leaflet js/L)
 
-(c/defn-subscription setup-leaflet deliver! [ref coords zoom-level marker-position]
+(c/defn-subscription setup-leaflet deliver! [ref coords zoom-level marker-position circle]
   (let [mp (.map leaflet (c/deref ref))
         tile-layer (.tileLayer leaflet "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
                                #js {:maxZoom 19,
@@ -16,16 +16,29 @@
       (let [marker (.marker leaflet (clj->js marker-position))]
         (.addTo marker mp)))
 
+    (when-let [[circle-center circle-radius] circle]
+      (let [crcl (.circle leaflet (clj->js circle-center) #js {:color "blue"
+                                                     :radius circle-radius})]
+        (.addTo crcl mp)))
+
     (fn [_]
       (.remove mp)
       )))
+
+(c/defn-item circle [view-coords zoom-level]
+  (c/with-state-as [circle-center circle-radius]
+    (c/with-ref
+      (fn [ref]
+        (c/fragment
+         (dom/div {:ref ref
+                   :style {:min-height 240}})
+         (setup-leaflet ref view-coords zoom-level nil [circle-center circle-radius]))))))
 
 (c/defn-item position [view-coords zoom-level]
   (c/with-state-as pin-coords
     (c/with-ref
       (fn [ref]
         (c/fragment
-         (dom/div {:id "the-map"
-                   :ref ref
-                   :style {:min-height 120}})
-         (setup-leaflet ref view-coords zoom-level pin-coords))))))
+         (dom/div {:ref ref
+                   :style {:min-height 240}})
+         (setup-leaflet ref view-coords zoom-level pin-coords nil))))))
