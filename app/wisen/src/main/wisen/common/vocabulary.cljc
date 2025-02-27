addf5b319e120325fb3febea8e2d2aa8a1d8797f
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-27 09:59:21 +0100

    Refactor: move more osm stuff to osm ns

---
+++ app/wisen/src/main/wisen/common/vocabulary.cljc
@@ -0,0 +1,5 @@
+(ns wisen.common.vocabulary)
+
+;; Special RDF vocabulary for our purposes
+
+(def wisen-osm-uri-predicate "https://wisen.active-group.de/osm-uri")
--- app/wisen/src/main/wisen/frontend/osm.cljs
+++ app/wisen/src/main/wisen/frontend/osm.cljs
@@ -2,12 +2,26 @@
   (:require [reacl-c.core :as c :include-macros true]
             [reacl-c.dom :as dom]
             [active.clojure.lens :as lens]
-            [reacl-c-basics.forms.core :as forms]
             [reacl-c-basics.ajax :as ajax]
-            [wisen.frontend.promise :as promise]
-            [wisen.frontend.rdf :as rdf]
-            [wisen.frontend.design-system :as ds]))
+            [wisen.frontend.tree :as tree]
+            [wisen.common.vocabulary :as vocabulary]
+            [clojure.string :as str]))
 
 (defn osm-lookup-request [osm-id]
   (ajax/GET (str "/osm/lookup/" osm-id)
             {:headers {:accept "application/ld+json"}}))
+
+(defn node-osm-uri [node]
+  (let [obj ((tree/node-object-for-predicate vocabulary/wisen-osm-uri-predicate) node)]
+    (when (tree/literal-string? obj)
+      (let [s (tree/literal-string-value obj)]
+        (when (str/starts-with? s "https://www.openstreetmap.org/")
+          s)))))
+
+(defn organization-do-link-osm [organization-node osm-id osm-place-node]
+  (lens/overhaul organization-node
+                 tree/node-properties
+                 (fn [old-properties]
+                   (-> old-properties
+                       (conj (tree/make-property "http://schema.org/location" osm-place-node))
+                       (conj (tree/make-property vocabulary/wisen-osm-uri-predicate (tree/make-literal-string osm-id)))))))
