feb88137a0bba6de471454051a38a2293843f78d
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-27 16:01:27 +0100

    Remove unused shorthands

--- app/wisen/src/main/wisen/frontend/rdf.cljs
+++ app/wisen/src/main/wisen/frontend/rdf.cljs
@@ -141,30 +141,3 @@
   (let [g (rdflib/graph)]
     (.addAll g (clj->js stmts))
     g))
-
-;; shorthands
-
-(defn resource-type [graph res]
-  (first
-   (subject-predicate-objects
-    graph
-    res
-    (make-symbol "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"))))
-
-(defn resource-name [graph res]
-  (when-let [name-node (first
-                        (subject-predicate-objects
-                         graph
-                         res
-                         (make-symbol "http://schema.org/name")))]
-    (when (literal-string? name-node)
-      (literal-string-value name-node))))
-
-(defn resource-description [graph res]
-  (when-let [node (first
-                        (subject-predicate-objects
-                         graph
-                         res
-                         (make-symbol "http://schema.org/description")))]
-    (when (literal-string? node)
-      (literal-string-value node))))
