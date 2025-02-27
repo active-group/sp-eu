ad7214aa56aadb04dbfd593021225c8a000e9468
Markus Schlegel <markus.schlegel@active-group.de>
2025-02-27 12:48:16 +0100

    Refactor: move some OSM parts to frontend

--- app/wisen/src/main/wisen/frontend/osm.cljs
+++ app/wisen/src/main/wisen/frontend/osm.cljs
@@ -7,16 +7,36 @@
             [wisen.common.vocabulary :as vocabulary]
             [clojure.string :as str]))
 
-(defn osm-lookup-request [osm-id]
-  (ajax/GET (str "/osm/lookup/" osm-id)
+(defn- osm-uri->osm-id [osm-uri]
+  (let [type-slash-id (subs osm-uri (count "https://www.openstreetmap.org/"))
+        l (str/split type-slash-id #"/|#")
+        type (first l)
+        id (second l)]
+    (str
+     (case type
+           "node" "N"
+           "way" "W"
+           "relation" "R")
+     id)))
+
+(osm-uri->osm-id "https://www.openstreetmap.org/way/629487031#map=20/48.5119874/9.0608092&layers=H")
+
+;; TODO: https://wisen.active-group.de in prod
+(def base "")
+
+(defn- osm-uri->wisen-osm-uri [osm-uri]
+  (str base "/osm/lookup/" (osm-uri->osm-id osm-uri)))
+
+#_(osm-uri->osm-id "https://www.openstreetmap.org/way/629487031")
+
+(defn osm-lookup-request [osm-uri]
+  (ajax/GET (osm-uri->wisen-osm-uri osm-uri)
             {:headers {:accept "application/ld+json"}}))
 
 (defn node-osm-uri [node]
   (let [obj ((tree/node-object-for-predicate vocabulary/wisen-osm-uri-predicate) node)]
     (when (tree/literal-string? obj)
-      (let [s (tree/literal-string-value obj)]
-        (when (str/starts-with? s "https://www.openstreetmap.org/")
-          s)))))
+      (tree/literal-string-value obj))))
 
 (defn organization-do-link-osm [organization-node osm-id osm-place-node]
   (lens/overhaul organization-node
