01228dbaadc9258084dfeccf9755ebb16b8645b3
Raoul Schlotterbeck <raoul.schlotterbeck@active-group.de>
2025-03-05 11:06:39 +0100

    Format ollama prompt

--- app/wisen/src/main/wisen/backend/llm.clj
+++ app/wisen/src/main/wisen/backend/llm.clj
@@ -8,9 +8,34 @@
   (:import (com.fasterxml.jackson.core JsonFactory JsonParser$Feature)
            (com.fasterxml.jackson.databind ObjectMapper)))
 
+(defn flatten-multiline-string [s]
+  (->> s
+       (clojure.string/split-lines)
+       (map clojure.string/trim)
+       (remove empty?)
+       (clojure.string/join " ")
+       (clojure.string/trim)))
+
+(def ensurances
+  "1. The output strictly follows Schema.org definitions. 2. All provided
+  information is included. 3. No additional information is added. 4. The output
+  is valid JSON-LD. 5. The response contains only the JSON-LD, with no
+  explanations, headers, or formatting outside the JSON structure.")
+
+(def guidelines
+  "Where applicable, stick to the following guide lines:
+    - for recurring events use `schema:Schedule`
+    - for addresses use `schema:PostalAddress`
+    - for `schema:startTime` or `schema:endTime` use a `xsd:time` or a `xsd:dateTime`.")
+
 (defn format-ollama-prompt [prompt]
-  ;; TODO: format (somehow requests fail with line breaks)
-  (format "Convert the following natural language description into JSON-LD using only Schema.org vocabulary. Ensure that: 1. The output strictly follows Schema.org definitions. 2. All provided information is included. 3. No additional information is added. 4. The output is valid JSON-LD. 5. The response contains only the JSON-LD, with no explanations, headers, or formatting outside the JSON structure. Input: %s Output: " prompt))
+  (flatten-multiline-string
+   (format
+    "Convert the following natural language description into JSON-LD using only
+    Schema.org vocabulary. Ensure that: %s %s Input: %s Output: "
+    ensurances
+    guidelines
+    prompt)))
 
 (defn make-ollama-request-string [prompt]
   (str "{\"model\": \"phi4\", \"stream\": false, \"prompt\": \"" prompt "\"}"))
