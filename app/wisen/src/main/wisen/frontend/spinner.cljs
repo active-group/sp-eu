ada46e068d44f4d8e2712c2f9d92aea00efe498d
Markus Schlegel <markus.schlegel@active-group.de>
2025-03-11 12:33:57 +0100

    Improve spinner (+ label)

--- app/wisen/src/main/wisen/frontend/spinner.cljs
+++ app/wisen/src/main/wisen/frontend/spinner.cljs
@@ -1,13 +1,23 @@
 (ns wisen.frontend.spinner
-  (:require [reacl-c.dom :as dom]))
+  (:require [reacl-c.dom :as dom :include-macros true]))
 
-(def main (dom/span {:style {:width "1em"
-                             :height "1em"
-                             :border-top "0.3ex solid currentColor"
-                             :border-right "0.3ex solid currentColor"
-                             :border-bottom "0.3ex solid transparent"
-                             :border-left "0.3ex solid currentColor"
-                             :border-radius "50%"
-                             :display "inline-block"
-                             :box-sizing "border-box"
-                             :animation "rotation 1s linear infinite"}}))
+(dom/defn-dom main [attrs & items]
+  (apply
+   dom/div
+   (dom/merge-attributes {:style {:display "flex"
+                                  :justify-content "center"
+                                  :gap "1em"
+                                  :align-items "center"
+                                  :font-style "italic"}}
+                         attrs)
+   (dom/span {:style {:width "1em"
+                      :height "1em"
+                      :border-top "0.3ex solid currentColor"
+                      :border-right "0.3ex solid currentColor"
+                      :border-bottom "0.3ex solid transparent"
+                      :border-left "0.3ex solid currentColor"
+                      :border-radius "50%"
+                      :display "inline-block"
+                      :box-sizing "border-box"
+                      :animation "rotation 1s linear infinite"}})
+   items))
