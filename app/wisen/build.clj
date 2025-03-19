9a28d61d989619cbd1085b4ac5be912a23f6e5ed
Raoul Schlotterbeck <raoul.schlotterbeck@active-group.de>
2025-03-19 15:34:51 +0100

    Try to build uberjar (WIP)

--- app/wisen/build.clj
+++ app/wisen/build.clj
@@ -4,22 +4,48 @@
 (def lib 'wisen/wisen)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
+(def output-dir "public/js")
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
 
 ;; delay to defer side effects (artifact downloads)
-(def basis (delay (b/create-basis {:project "deps.edn"})))
+(def basis (delay (b/create-basis {:project "deps.edn"
+                                   :aliases [:backend]})))
+
+(def cljs-basis (delay (b/create-basis {:project "deps.edn"
+                                        :aliases [:shadow-cljs]})))
+
 
 (defn clean [_]
-  (b/delete {:path "target"}))
+  (b/delete {:path class-dir})
+  (b/delete {:path output-dir}))
 
-(defn uber [_]
-  (clean nil)
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
+(defn shadow-cljs [_]
+  (let [cmds (b/java-command {:basis @cljs-basis
+                              :main 'clojure.main
+                              :main-args ["-m"
+                                          "shadow.cljs.devtools.cli"
+                                          "release"
+                                          "frontend"]})]
+    (b/process cmds)))
+
+(defn compile-clj [_]
   (b/compile-clj {:basis @basis
                   :ns-compile '[wisen.backend.main]
-                  :class-dir class-dir})
+                  :class-dir class-dir}))
+
+(defn copy-dirs [_]
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir}))
+
+(defn make-uber [_]
   (b/uber {:class-dir class-dir
            :uber-file uber-file
            :basis @basis
            :main 'wisen.backend.main}))
+
+(defn uber [_]
+  (clean nil)
+  (copy-dirs nil)
+  (shadow-cljs nil)
+  (compile-clj nil)
+  (make-uber nil))
