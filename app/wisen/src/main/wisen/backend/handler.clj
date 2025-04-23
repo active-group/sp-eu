cd7ae1df5a665828bd2bdab7b1f70d2ef4e35789
Markus Schlegel <markus.schlegel@active-group.de>
2025-04-23 15:11:35 +0200

    Remove unpkg leaflet js

--- app/wisen/src/main/wisen/backend/handler.clj
+++ app/wisen/src/main/wisen/backend/handler.clj
@@ -197,10 +197,7 @@
                    [:link {:rel "stylesheet"
                            :href "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
                            :integrity "sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-                           :crossorigin ""}]
-                   [:script {:src "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-                             :integrity "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-                             :crossorigin ""}]]
+                           :crossorigin ""}]]
                   [:body
                    [:div {:id "main"}]
                    [:script {:type "text/javascript"} prefix/set-prefix-code]
