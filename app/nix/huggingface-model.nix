ef536ef3006c553ffdfe762a53be9372706112eb
Raoul Schlotterbeck <raoul.schlotterbeck@active-group.de>
2025-05-15 15:07:33 +0200

    Nixify embedding model

---
+++ app/nix/huggingface-model.nix
@@ -0,0 +1,41 @@
+{ pkgs ? import <nixpkgs> {}
+, modelName ? "BAAI/bge-m3"
+, modelRevision ? "5617a9f61b028005a4858fdac845db406aefb181"
+}:
+
+let
+  pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+    torch
+    transformers
+  ]);
+
+  safeModelName = builtins.replaceStrings ["/"] ["-"] modelName;
+
+  conversionScript = ./convert_model.py;
+
+in pkgs.stdenv.mkDerivation {
+  pname = "${safeModelName}-torchscript";
+  version = "1.0.0";
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [ pythonEnv ];
+
+  buildPhase = ''
+    export HF_HOME="$TMPDIR/hf_home"
+    mkdir -p $HF_HOME
+    cp ${conversionScript} ./convert_model.py
+
+    python ./convert_model.py --model-name "${modelName}" --model-revision "${modelRevision}"
+  '';
+
+  outputHashMode = "recursive";
+  outputHashAlgo = "sha256";
+  outputHash = "sha256:1b1lmjb7q6c6rg4bbcpxvlswqx31yx5hqlm99yx5bdc2j7vaqn33";
+
+  meta = with pkgs.lib; {
+    description = "TorchScript version of ${modelName} for use with DJL";
+    homepage = "https://huggingface.co/${modelName}";
+    license = licenses.mit;
+  };
+}
