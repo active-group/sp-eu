e278c36dd5e64b5c04b109c2f0686bdfa9200538
Raoul Schlotterbeck <raoul.schlotterbeck@active-group.de>
2025-05-15 16:43:12 +0200

    Don't use FOD

--- app/nix/huggingface-model.nix
+++ app/nix/huggingface-model.nix
@@ -29,9 +29,9 @@ in pkgs.stdenv.mkDerivation {
     python ./convert_model.py --model-name "${modelName}" --model-revision "${modelRevision}"
   '';
 
-  outputHashMode = "recursive";
-  outputHashAlgo = "sha256";
-  outputHash = "sha256:1b1lmjb7q6c6rg4bbcpxvlswqx31yx5hqlm99yx5bdc2j7vaqn33";
+  # outputHashMode = "recursive";
+  # outputHashAlgo = "sha256";
+  # outputHash = "sha256-iwV9YchzrzSnF95E5ZqDI2gI6oS2dA3KZQ3b8DCvk+I=";
 
   meta = with pkgs.lib; {
     description = "TorchScript version of ${modelName} for use with DJL";
