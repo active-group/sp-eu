838ecd5a1ece1779656205ea1848f2e1b1bf4b12
Markus Schlegel <markus.schlegel@active-group.de>
2025-05-26 14:41:16 +0200

    Pressed state

--- app/wisen/src/main/wisen/backend/handler.clj
+++ app/wisen/src/main/wisen/backend/handler.clj
@@ -216,6 +216,8 @@
                                         font-family: 'Helvetica Neue', Helvetica, sans-serif;}
                             ul, ol {margin: 0; padding: 0; padding-left: 1.4em;}
                             hr {margin: 0; padding: 0; border: 0; border-bottom: 1px solid gray;}
+                            a, button { position: relative; }
+                            a:active, button:active { top: 1px; opacity: 0.7; }
 
     @keyframes rotation {
     0% {
