2418b7fa1e7aa3a62a90e72a55a90536c022baf2
Johannes Maier <johannes.maier@mailbox.org>
2025-06-05 15:05:58 +0200

    setup: add development vm
    After some adjustments this could be used as a replacement for Docker as
    part of the development setup (entirely).

--- nix/nixos-configurations/prod.nix
+++ nix/nixos-configurations/prod.nix
@@ -10,6 +10,7 @@ in
 {
   imports = [
     ./hardware-configuration.nix
+    ./vm-base.nix
   ];
 
   boot.loader.grub = {
@@ -34,8 +35,6 @@ in
     defaults.email = "admin@active-group.de";
   };
 
-  virtualisation.docker.enable = true;
-
   systemd.services = {
     sp-eu = {
       wantedBy = [ "multi-user.target" ];
@@ -48,60 +47,9 @@ in
         TimeoutStartSec = "0";
       };
     };
-
-    keycloak = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "docker.service" ];
-      description = "Start SP-EU keycloak docker container.";
-      serviceConfig = {
-        User = "root";
-        WorkingDirectory = "/root/wisen/keycloak";
-        ExecStart = ''
-          ${pkgs.docker}/bin/docker run -p 8080:8080 \
-            -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=agkeycloac07 \
-            -e KC_PROXY_HEADERS=xforwarded \
-            -e KC_HOSTNAME=https://sp-eu.ci.active-group.de/cloak \
-            -e KC_HTTPS_CERTIFICATE_FILE=/opt/keycloak/data/import/fullchain.pem \
-            -e KC_HTTPS_CERTIFICATE_KEY_FILE=/opt/keycloak/data/import/key.pem \
-            -v /root/wisen/keycloak:/opt/keycloak/data/import \
-            quay.io/keycloak/keycloak@sha256:044a457e04987e1fff756be3d2fa325a4ef420fa356b7034ecc9f1b693c32761 \
-            start-dev --import-realm
-        '';
-      };
-    };
   };
 
   services = {
-    postgresql.enable = false;
-    keycloak = {
-      enable = false;
-
-      initialAdminPassword = "agkeycloac07";
-
-      realmFiles = [ (pkgs.writeText "speu-realm.json" "") ];
-
-      database = {
-        type = "postgresql";
-        createLocally = true;
-
-        username = "keycloak";
-        passwordFile = "/root/keycloak_psql_pass";
-      };
-
-      settings = {
-        hostname = "https://sp-eu.ci.active-group.de/cloak";
-        http-relative-path = "/cloak";
-        http-port = 38080;
-        http-enabled = true;
-        proxy-headers = "xforwarded";
-      };
-    };
-
-    ollama = {
-      enable = true;
-      loadModels = [ "phi4" ];
-    };
-
     nginx = {
       enable = true;
       virtualHosts.${domain} = {
@@ -124,26 +72,12 @@ in
         enableACME = true;
         forceSSL = true;
       };
-      # recommendedProxySettings = true;
-    };
-
-    riemann-tools = {
-      enableHealth = true;
-      riemannHost = "riemann.active-group.de";
-    };
-
-    openssh = {
-      enable = true;
-      settings.PermitRootLogin = "yes";
     };
   };
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkJUKlZpli3wOU3DLI6qEm1286CFOOmwnTUWOzalmMx ci@gitlab"
   ];
-  nix = {
-    package = pkgs.nixVersions.stable;
-    extraOptions = "experimental-features = nix-command flakes";
-  };
+
   system.stateVersion = "24.11";
 }
