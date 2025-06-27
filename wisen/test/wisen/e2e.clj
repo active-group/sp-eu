ba76647b5e1235e38c2fa644e9acfd177c6ea1c0
Johannes Maier <johannes.maier@mailbox.org>
2025-06-27 17:52:12 +0200

    e2e: make app url configurable

--- wisen/test/wisen/e2e.clj
+++ wisen/test/wisen/e2e.clj
@@ -2,13 +2,14 @@
   (:require [etaoin.api :as e])
   (:gen-class))
 
-(defn -main [& _args]
+(defn -main [& args]
   (let [driver (e/chrome
                 {:path-driver "chromedriver"
-                 :path-browser (System/getenv "CHROME_BIN")})]
+                 :path-browser (System/getenv "CHROME_BIN")})
+        url (or (first args) "http://localhost:4321")]
     (doto driver
       (e/driver-type)
-      (e/go "http://localhost:4321")
+      (e/go url)
       (e/wait-visible {:tag :input :type :text :id "username"})
       (e/fill {:tag :input :type :text :id "username"} "admin-user")
       (e/fill {:tag :input :type :password} "password")
