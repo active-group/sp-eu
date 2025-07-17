e5502c3ae48df8b6a13fe6c126a3d0aa4958c6e0
Markus Schlegel <markus.schlegel@active-group.de>
2025-07-17 09:54:12 +0200

    Skip already skolemized

--- wisen/skolemize-all.sh
+++ wisen/skolemize-all.sh
@@ -2,5 +2,11 @@
 
 set -e
 
-for file in ../jsonld/*.jsonld; do ./skolemize.sh "$file"; done
+for file in ../jsonld/*.jsonld; do
+    skolemized="${file}.skolemized"
+
+    if [[ ! -e "$skolemized" ]]; then
+      ./skolemize.sh "$file"
+    fi
+done
 
