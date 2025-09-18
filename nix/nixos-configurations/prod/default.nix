d16a1be2c7101f8cf2bfc61c95deb8a63f5ae142
Markus Schlegel <markus.schlegel@active-group.de>
2025-09-18 14:31:01 +0200

    Add my ssh public key

--- nix/nixos-configurations/prod/default.nix
+++ nix/nixos-configurations/prod/default.nix
@@ -49,6 +49,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkJUKlZpli3wOU3DLI6qEm1286CFOOmwnTUWOzalmMx ci@gitlab"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCnQ4OO6RiQWDYuiD01RxxPAQL6TQ64TILfBdVnvpwWo2ksUjsD5ZaAyB57AiR5NqRCtpUEuPwRsDQ8rfgZfVRiJZBs6JnCJD1yQYvIMWvuOJtDAVHAWo8XPfpS/nyog5E8Qv9u9FTKZQgw7z7ZGCDM5yG+tR94Y6g84Q4+8UW/g+HJvxX/6LKNpGdDZEkY5kty+07fltxPlozenpl7/v7F6yf2eLqa6Iy2XqhwlJeG1A/XnRKvIztvY6SUOCLQmTJm0Q4iCAoef+ub07uOZNBhDT3evsIwhqdGFUM9hXZ9lm0KloHlH2Rop4enkFfwMf0pYS3vfvLFYpfPAbmjQMFD marki"
   ];
 
   system.stateVersion = "24.11";
