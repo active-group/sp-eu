285f4164deac14494b4d8921f7cddb40dc34d8b1
Markus Schlegel <markus.schlegel@active-group.de>
2025-09-19 15:37:35 +0200

    Automatically adapt resource view to current head / base-commit-id

--- wisen/src/main/wisen/frontend/resource.cljs
+++ wisen/src/main/wisen/frontend/resource.cljs
@@ -2,6 +2,7 @@
   (:require [reacl-c.core :as c :include-macros true]
             [reacl-c.dom :as dom :include-macros true]
             [reacl-c.main :as cmain]
+            [active.clojure.lens :as lens]
             [wisen.frontend.util :as util]
             [reacl-c-basics.ajax :as ajax]
             [wisen.frontend.editor :as editor]
@@ -23,30 +24,31 @@
   (ds/padded-2
    {:style {:overflow-y "auto"}}
    (c/isolate-state
-    nil
-    (c/with-state-as result
-      (if (nil? result)
-        (c/fragment
-         (spinner/main)
-         (-> (util/load-json-ld
-              (resource-request (context/commit-id ctx) resource-id))
-             (c/handle-action (fn [st ac]
-                                (if (success? ac)
-                                  (edit-tree/graph->edit-tree
-                                   (success-value ac))
-                                  (assert false "TODO: implement error handling"))))))
-        ;; else
-        (dom/div
-         (editor/edit-tree-component ctx nil true false nil)
-         (let [show-commit-bar? (not-empty (edit-tree/edit-tree-changeset result))]
-           (dom/div
-            {:style {:border ds/border
-                     :position "sticky"
-                     :bottom (if show-commit-bar?
-                               "10px"
-                               "-60px")
-                     :transition "bottom 0.3s"
-                     :border-radius "4px"
-                     :background "#ddd"
-                     :z-index "999"}}
-            (commit/main ctx)))))))))
+    {}
+    (c/focus (lens/member (context/commit-id ctx))
+             (c/with-state-as result
+               (if (nil? result)
+                 (c/fragment
+                  (spinner/main)
+                  (-> (util/load-json-ld
+                       (resource-request (context/commit-id ctx) resource-id))
+                      (c/handle-action (fn [st ac]
+                                         (if (success? ac)
+                                           (edit-tree/graph->edit-tree
+                                            (success-value ac))
+                                           (assert false "TODO: implement error handling"))))))
+                 ;; else
+                 (dom/div
+                  (editor/edit-tree-component ctx nil true false nil)
+                  (let [show-commit-bar? (not-empty (edit-tree/edit-tree-changeset result))]
+                    (dom/div
+                     {:style {:border ds/border
+                              :position "sticky"
+                              :bottom (if show-commit-bar?
+                                        "10px"
+                                        "-60px")
+                              :transition "bottom 0.3s"
+                              :border-radius "4px"
+                              :background "#ddd"
+                              :z-index "999"}}
+                     (commit/main ctx))))))))))
