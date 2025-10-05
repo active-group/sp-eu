0fe7946af7f249ad94426e77497a70ed3d40fe9d
Markus Schlegel <markus.schlegel@active-group.de>
2025-10-05 20:49:57 +0200

    Fix

--- wisen/src/main/wisen/backend/resource.clj
+++ wisen/src/main/wisen/backend/resource.clj
@@ -1,6 +1,6 @@
 (ns wisen.backend.resource
-  (:require [wisen.common.prefix :as prefix
-             wisen.common.urn :as urn]))
+  (:require [wisen.common.prefix :as prefix]
+            [wisen.common.urn :as urn]))
 
 (defn uri-for-resource-id [id]
   (if (urn/urn? id)
